297df957f3d06c9adbe83c51517ff857d07e25bf
Mick F <mick@mickf.net>
2021-01-14 23:46:57 +0100

    feat: basic example of using the swift-html package

--- Package.swift
+++ Package.swift
@@ -3,18 +3,24 @@
 
 import PackageDescription
 
+// Dependencies can be hard to read.
+// 📜 https://www.timc.dev/posts/understanding-swift-packages/
+
 let package = Package(
     name: "kids-html",
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(name: "Html", url: "https://github.com/pointfreeco/swift-html.git", from: "0.3.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "kids-html",
-            dependencies: []
+            dependencies: [
+                .product(name: "Html", package: "Html")
+            ]
         ),
         .testTarget(
             name: "kids-htmlTests",
--- Sources/kids-html/main.swift
+++ Sources/kids-html/main.swift
@@ -1,1 +1,16 @@
-print("Hello, world!")
+import Foundation
+import Html
+
+let document: Node = .document(
+    .html(
+        .body(
+            .h1("Welcome!"),
+            .p("You’ve found our site!")
+        )
+    )
+)
+
+let html = render(document)
+let outputURL = URL(fileURLWithPath: "./output.html")
+print(outputURL.path)
+try html.write(to: outputURL, atomically: true, encoding: .utf8)
